48a0fff4b5bcd15109b135b97977ef55cada263f
Thisuru <thisurudk@gmail.com>
2019-12-04 21:03:53 +0530

    questionnare query changed

--- web/DB Files/web_db.sql
+++ web/DB Files/web_db.sql
@@ -206,77 +206,6 @@ CREATE TABLE if not exists `patient_marks`(
 
 
 CREATE TABLE if not exists `questionnaire`(
-   `qNo` int(11) NOT NULL auto_increment,
-  `question` varchar(50) NOT NULL,
-  `answerOne` varchar(50) NOT NULL,
-  `answerTwo` varchar(50) NOT NULL,
-  `answerThree` varchar(50) NOT NULL,
-  `answerFour` varchar(50) NOT NULL,
-  PRIMARY KEY  (`qNo`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answerThree`, `answerFour`) VALUES
-('1', 'Question 1', 'I do not feel sad', 'I feel sad', 'I am sad all the time and I can not snap out of it', 'I am so sad and unhappy that I can not stand it');
-
-INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answerThree`, `answerFour`) VALUES
-('2', 'Question 2', 'I am not particularly discouraged about the future', 'I feel discouraged about the future', 'I feel I have nothing to look forward to', 'I feel the future is hopeless and that things cannot improve');
-
-INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answerThree`, `answerFour`) VALUES
-('3', 'Question 3', 'I do not feel like a failure', 'I feel I have failed more than the average person', 'As I look back on my life, all I can see is a lot of failures', 'I feel I am a complete failure as a person');
-
-INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answerThree`, `answerFour`) VALUES
-('4', 'Question 4', 'I get as much satisfaction out of things as I used to', 'I don not enjoy things the way I used to', 'I don not get real satisfaction out of anything anymore', 'I am dissatisfied or bored with everything');
-
-INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answerThree`, `answerFour`) VALUES
-('5', 'Question 5', 'I don not feel particularly guilty', 'I feel guilty a good part of the time', 'I feel quite guilty most of the time', 'I feel guilty all of the time');
-
-INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answerThree`, `answerFour`) VALUES
-('6', 'Question 6', 'I don not feel I am being punished', 'I feel I may be punished', 'I expect to be punished', 'I feel I am being punished');
-
-INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answerThree`, `answerFour`) VALUES
-('7', 'Question 7', 'I don not feel disappointed in myself', 'I am disappointed in myself', 'I am disgusted with myself', 'I hate myself');
-
-INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answerThree`, `answerFour`) VALUES
-('8', 'Question 8', 'I don not feel I am any worse than anybody else', 'I am critical of myself for my weaknesses or mistakes', 'I blame myself all the time for my faults', 'I blame myself for everything bad that happens');
-
-INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answerThree`, `answerFour`) VALUES
-('9', 'Question 9', 'I don not have any thoughts of killing myself', 'I have thoughts of killing myself, but I would not carry them out', 'I would like to kill myself', 'I would kill myself if I had the chance');
-
-INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answerThree`, `answerFour`) VALUES
-('10', 'Question 10', 'I don not cry any more than usual', 'I cry more now than I used to', 'I cry all the time now', 'I used to be able to cry, but now I can not cry even though I want to');
-
-INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answerThree`, `answerFour`) VALUES
-('11', 'Question 11', 'I am no more irritated by things than I ever was', 'I am slightly more irritated now than usual', 'I am quite annoyed or irritated a good deal of the time', 'I feel irritated all the time');
-
-INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answerThree`, `answerFour`) VALUES
-('12', 'Question 12', 'I am no more irritated by things than I ever was', 'I am slightly more irritated now than usual', 'I am quite annoyed or irritated a good deal of the time', 'I feel irritated all the time');
-
-INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answerThree`, `answerFour`) VALUES
-('13', 'Question 13', 'I make decisions about as well as I ever could', 'I put off making decisions more than I used to', 'I have greater difficulty in making decisions more than I used to', 'I can not make decisions at all anymore');
-
-INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answerThree`, `answerFour`) VALUES
-('14', 'Question 14', 'I don not feel that I look any worse than I used to', 'I am worried that I am looking old or unattractive', 'I feel there are permanent changes in my appearance that make me look unattractive', 'I believe that I look ugly');
-
-INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answerThree`, `answerFour`) VALUES
-('15', 'Question 15', 'I can work about as well as before', 'It takes an extra effort to get started at doing something', 'I have to push myself very hard to do anything', 'I can not do any work at all');
-
-INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answerThree`, `answerFour`) VALUES
-('16', 'Question 16', 'I can sleep as well as usual', 'I don not sleep as well as I used to', 'I wake up 1-2 hours earlier than usual and find it hard to get back to sleep', 'I wake up several hours earlier than I used to and cannot get back to sleep');
-
-INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answerThree`, `answerFour`) VALUES
-('17', 'Question 17', 'I don not get more tired than usual', 'I get tired more easily than I used to', 'I get tired from doing almost anything', 'I am too tired to do anything');
-
-INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answerThree`, `answerFour`) VALUES
-('18', 'Question 18', 'My appetite is no worse than usual', 'My appetite is not as good as it used to be', 'My appetite is much worse now', 'I have no appetite at all anymore');
-
-INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answerThree`, `answerFour`) VALUES
-('19', 'Question 19', 'I haven not lost much weight, if any, lately', 'I have lost more than five pounds', 'I have lost more than ten pounds', 'I have lost more than fifteen pounds');
-
-INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answerThree`, `answerFour`) VALUES
-('20', 'Question 20', 'I am no more worried about my health than usual', 'I am worried about physical problems like aches, pains, upset stomach, or constipation', 'I am very worried about physical problems and it is hard to think of much else', 'I am so worried about my physical problems that I cannot think of anything else');
-
-INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answerThree`, `answerFour`) VALUES
-('21', 'Question 21', 'I have not noticed any recent change in my interest in sex', 'I am less interested in sex than I used to be', 'I have almost no interest in sex', 'I have lost interest in sex completely');
   `questionId` int(11) NOT NULL auto_increment,
   `question` varchar(250) NOT NULL,
   `answer1` varchar(250) NOT NULL,
@@ -287,8 +216,73 @@ INSERT INTO `questionnaire` (`qNo`, `question`, `answerOne`, `answerTwo`, `answe
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
-('TestQ', 'testA1', 'testA2', 'test3', 'test4'),
-('TestQ', 'testA1', 'testA2', 'test3', 'test4');
+('Question 1', 'I do not feel sad', 'I feel sad', 'I am sad all the time and I can not snap out of it', 'I am so sad and unhappy that I can not stand it');
+
+INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
+('Question 2', 'I am not particularly discouraged about the future', 'I feel discouraged about the future', 'I feel I have nothing to look forward to', 'I feel the future is hopeless and that things cannot improve');
+
+INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
+('Question 3', 'I do not feel like a failure', 'I feel I have failed more than the average person', 'As I look back on my life, all I can see is a lot of failures', 'I feel I am a complete failure as a person');
+
+INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
+('Question 4', 'I get as much satisfaction out of things as I used to', 'I don not enjoy things the way I used to', 'I don not get real satisfaction out of anything anymore', 'I am dissatisfied or bored with everything');
+
+INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
+('Question 5', 'I don not feel particularly guilty', 'I feel guilty a good part of the time', 'I feel quite guilty most of the time', 'I feel guilty all of the time');
+
+INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
+('Question 6', 'I don not feel I am being punished', 'I feel I may be punished', 'I expect to be punished', 'I feel I am being punished');
+
+INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
+('Question 7', 'I don not feel disappointed in myself', 'I am disappointed in myself', 'I am disgusted with myself', 'I hate myself');
+
+INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
+('Question 8', 'I don not feel I am any worse than anybody else', 'I am critical of myself for my weaknesses or mistakes', 'I blame myself all the time for my faults', 'I blame myself for everything bad that happens');
+
+INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
+('Question 9', 'I don not have any thoughts of killing myself', 'I have thoughts of killing myself, but I would not carry them out', 'I would like to kill myself', 'I would kill myself if I had the chance');
+
+INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
+('Question 10', 'I don not cry any more than usual', 'I cry more now than I used to', 'I cry all the time now', 'I used to be able to cry, but now I can not cry even though I want to');
+
+INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
+('Question 11', 'I am no more irritated by things than I ever was', 'I am slightly more irritated now than usual', 'I am quite annoyed or irritated a good deal of the time', 'I feel irritated all the time');
+
+INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
+('Question 12', 'I am no more irritated by things than I ever was', 'I am slightly more irritated now than usual', 'I am quite annoyed or irritated a good deal of the time', 'I feel irritated all the time');
+
+INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
+('Question 13', 'I make decisions about as well as I ever could', 'I put off making decisions more than I used to', 'I have greater difficulty in making decisions more than I used to', 'I can not make decisions at all anymore');
+
+INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
+('Question 14', 'I don not feel that I look any worse than I used to', 'I am worried that I am looking old or unattractive', 'I feel there are permanent changes in my appearance that make me look unattractive', 'I believe that I look ugly');
+
+INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
+('Question 15', 'I can work about as well as before', 'It takes an extra effort to get started at doing something', 'I have to push myself very hard to do anything', 'I can not do any work at all');
+
+INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
+('Question 16', 'I can sleep as well as usual', 'I don not sleep as well as I used to', 'I wake up 1-2 hours earlier than usual and find it hard to get back to sleep', 'I wake up several hours earlier than I used to and cannot get back to sleep');
+
+INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
+('Question 17', 'I don not get more tired than usual', 'I get tired more easily than I used to', 'I get tired from doing almost anything', 'I am too tired to do anything');
+
+INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
+('Question 18', 'My appetite is no worse than usual', 'My appetite is not as good as it used to be', 'My appetite is much worse now', 'I have no appetite at all anymore');
+
+INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
+('Question 19', 'I haven not lost much weight, if any, lately', 'I have lost more than five pounds', 'I have lost more than ten pounds', 'I have lost more than fifteen pounds');
+
+INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
+('Question 20', 'I am no more worried about my health than usual', 'I am worried about physical problems like aches, pains, upset stomach, or constipation', 'I am very worried about physical problems and it is hard to think of much else', 'I am so worried about my physical problems that I cannot think of anything else');
+
+INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
+('Question 21', 'I have not noticed any recent change in my interest in sex', 'I am less interested in sex than I used to be', 'I have almost no interest in sex', 'I have lost interest in sex completely');
+  
+  
+
+-- INSERT INTO `questionnaire` (`question`, `answer1`, `answer2`, `answer3`, `answer4`) VALUES
+-- ('TestQ', 'testA1', 'testA2', 'test3', 'test4'),
+-- ('TestQ', 'testA1', 'testA2', 'test3', 'test4');
 
 
 CREATE TABLE if not exists `answers`(
